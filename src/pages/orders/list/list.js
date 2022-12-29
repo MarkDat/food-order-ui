48c43eb21681ad8b5ccb9771d8c4573870d4daff
Dat Luong <dat.luong@enlabsoftware.com>
2022-12-29 16:15:53 +0700

    Add

--- src/pages/orders/list/list.js
+++ src/pages/orders/list/list.js
@@ -1,117 +1,108 @@
 import DataGrid, {
-	Column, FilterRow,
-	Lookup, Pager,
-	Paging
-} from 'devextreme-react/data-grid';
+	Column,
+	FilterRow,
+	Lookup,
+	Pager,
+	Paging,
+} from "devextreme-react/data-grid";
 
-import { ActionCellTemplate } from '@components/shared';
-import './list.scss';
+import { ActionCellTemplate } from "@components/shared";
+import "./list.scss";
 
 export default function OrderList() {
+	const onEditClicked = (e, data) => {
+		console.log(data);
+	};
 
-	const onEditClicked = (e) => {
-		console.log(e);
-	}
-
-	const onDeleteClicked = (e) => {
-		console.log(e);
-	}
+	const onDeleteClicked = (e, data) => {
+		console.log(data);
+	};
 
 	return (
 		<>
 			<DataGrid
-				className={'dx-card wide-card'}
+				className={"dx-card wide-card"}
 				dataSource={dataSource}
 				showBorders={false}
-				focusedRowEnabled={true}
+				focusedRowEnabled={false}
 				defaultFocusedRowIndex={0}
 				columnAutoWidth={true}
-				columnHidingEnabled={true}
+				columnHidingEnabled={false}
+				hoverStateEnabled={true}
 			>
 				<Paging defaultPageSize={10} />
 				<Pager showPageSizeSelector={true} showInfo={true} />
 				<FilterRow visible={true} />
 
-				{/* <Column dataField={'Task_ID'} width={90} hidingPriority={2} caption={'Mã nhà hàng'} alignment='center'/> */}
 				<Column
-					dataField={'Task_Subject'}
-					width={190}
-					caption={'Tên nhà hàng'}
-					hidingPriority={8}
+					dataField={"Task_ID"}
+					width={100}
+					caption={"Mã nhà hàng"}
+					alignment="center"
 				/>
 				<Column
-					dataField={'Task_Status'}
-					caption={'Trạng thái'}
-					hidingPriority={6}
+					dataField={"Task_Subject"}
+					width={300}
+					caption={"Tên nhà hàng"}
+				/>
+				<Column dataField={"Task_Status"} caption={"Trạng thái"} />
+				<Column
+					dataField={"Task_Status"}
+					caption={"Người tạo đơn"}
+					alignment={'left'}
 				/>
 				<Column
-					dataField={'Task_Priority'}
-					caption={'Số người đặt'}
-					hidingPriority={5}
-				>
+					dataField={"Task_Start_Date"}
+					caption={"Ngày tạo đơn"}
+					dataType={"date"}
+				/>
+				<Column dataField={"Task_Priority"} caption={"Số người đã đặt"} width={100}>
 					<Lookup
 						dataSource={priorities}
-						valueExpr={'value'}
-						displayExpr={'name'}
+						valueExpr={"value"}
+						displayExpr={"name"}
 					/>
 				</Column>
-				<Column
-					dataField={'Task_Start_Date'}
-					caption={'Ngày tạo đơn'}
-					dataType={'date'}
-					hidingPriority={3}
-				/>
-				<Column
-					dataField={'Task_Due_Date'}
-					caption={'Due Date'}
-					dataType={'date'}
-					hidingPriority={4}
-				/>
-				<Column
-					dataField={'Task_Priority'}
-					caption={'Priority'}
-					name={'Priority'}
-					hidingPriority={1}
-				/>
-				<Column
-					dataField={'Task_Completion'}
-					caption={'Completion'}
-					hidingPriority={0}
-				/>
 
 				<Column
 					width={60}
 					fixed={true}
 					fixedPosition="right"
-					cellRender={(e) => <ActionCellTemplate event={e} onEditClicked={onEditClicked} onDeleteClicked={onDeleteClicked} />}
+					cellRender={(e) => (
+						<ActionCellTemplate
+							event={e}
+							onEditClicked={onEditClicked}
+							onDeleteClicked={onDeleteClicked}
+						/>
+					)}
 				/>
 			</DataGrid>
 		</>
-	)
-};
+	);
+}
 
 const dataSource = {
 	store: {
-		type: 'odata',
-		key: 'Task_ID',
-		url: 'https://js.devexpress.com/Demos/DevAV/odata/Tasks'
+		type: "odata",
+		key: "Task_ID",
+		url: "https://js.devexpress.com/Demos/DevAV/odata/Tasks",
 	},
-	expand: 'ResponsibleEmployee',
+	expand: "ResponsibleEmployee",
 	select: [
-		'Task_ID',
-		'Task_Subject',
-		'Task_Start_Date',
-		'Task_Due_Date',
-		'Task_Status',
-		'Task_Priority',
-		'Task_Completion',
-		'ResponsibleEmployee/Employee_Full_Name'
-	]
+		"Task_ID",
+		"Task_Subject",
+		"Task_Start_Date",
+		"Task_Due_Date",
+		"Task_Status",
+		"Task_Priority",
+		"Task_Completion",
+		"ResponsibleEmployee/Employee_Full_Name",
+	],
 };
 
 const priorities = [
-	{ name: 'High', value: 4 },
-	{ name: 'Urgent', value: 3 },
-	{ name: 'Normal', value: 2 },
-	{ name: 'Low', value: 1 }
+	{ name: "High", value: 4 },
+	{ name: "Urgent", value: 3 },
+	{ name: "Normal", value: 2 },
+	{ name: "Low", value: 1 },
 ];
